15cbbbe77265dff10ecf26d97c9357af87c08983
leandrobandeira <leoviolinist22@gmail.com>
2024-07-22 17:18:59 -0300

    adding training draw circle

--- icg-init/hello_world.cpp
+++ icg-init/hello_world.cpp
@@ -1,4 +1,6 @@
 #include <GL/glut.h>
+#include <math.h>
+
 
 GLint WINDOW_WIDTH = 500;
 GLint WINDOW_HEIGHT = 500;
@@ -8,21 +10,24 @@ void draw(){
     glClear(GL_COLOR_BUFFER_BIT);
 
     /* define a cor do pixel */
-    glColor3f(1.0f,0.0f,0.0f);
     /* Define tamanho do pixel */
     glPointSize(3.0); 
     glLineWidth(3.0);
     /* GL_LINE considera apenas pares e GL_LINE_STRIP considera os três*/
     /* O GL_LINE_LOOP conecta todos os vértices */
     /* GL_TRIANGLES constroi um triangulo sólido (preenchido)*/
-    
-    glBegin(GL_TRIANGLES);
-        glVertex2i(250, 100);
-        glColor3f(0.0f,1.0f,0.0f);
-        glVertex2i(100, 400);
-        glColor3f(0.0f,0.0f,1.0f);
-        glVertex2i(400, 400);
+	    
+    int number = 40;
+    float radius = 0.4f;
+    float twopi = 2 * M_PI;
 
+    glBegin(GL_TRIANGLE_FAN);
+        
+    	glColor3f(0.0f,0.0f,0.0f);
+    	glVertex2f(250.0f, 250.0f);
+	for(int i = 0; i < number; i++){
+		glVertex2f(radius*cosf(i*twopi/number), radius*sinf(i*twopi/number));
+	}
 
     glEnd();
     /* Internamente está branco, porém no display ainda não está, devemos dar refresh na tela */
@@ -44,7 +49,8 @@ void windowInfo(){
     /* Define o modo da matriz para tratar transformações */
     glMatrixMode(GL_MODELVIEW);
     /* Define o padrão das posições dos pixels mais a esquerda, mais a direita e cima e baixo */
-    gluOrtho2D(0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);    
+    //gluOrtho2D(WINDOW_WIDTH/2, WINDOW_WIDTH/2, WINDOW_HEIGHT/2, WINDOW_HEIGHT/2);    
+    glLoadIdentity();
 }
 
 
